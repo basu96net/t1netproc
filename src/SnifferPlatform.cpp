c4839771627e300832f38c60d928b870765a5d2c
mono <mono@client-01>
2017-11-29 23:27:37 +0330

    god fiux

--- src/SnifferPlatform.cpp
+++ src/SnifferPlatform.cpp
@@ -5,108 +5,6 @@ UpdateConnectionTableCallback(u_char *args, const struct pcap_pkthdr *header, co
 
 
 
-
-
-/*
- * print data in rows of 16 bytes: offset   hex   ascii
- *
- * 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
- */
-void
-SnifferPlatform::print_hex_ascii_line(const u_char *payload, int len, int offset)
-{
-
-	int i;
-	int gap;
-	const u_char *ch;
-
-	/* offset */
-	printf("%05d   ", offset);
-
-	/* hex */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		printf("%02x ", *ch);
-		ch++;
-		/* print extra space after 8th byte for visual aid */
-		if (i == 7)
-			printf(" ");
-	}
-	/* print space to handle line less than 8 bytes */
-	if (len < 8)
-		printf(" ");
-
-	/* fill hex gap with spaces if not full line */
-	if (len < 16) {
-		gap = 16 - len;
-		for (i = 0; i < gap; i++) {
-			printf("   ");
-		}
-	}
-	printf("   ");
-
-	/* ascii (if printable) */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		if (isprint(*ch))
-			printf("%c", *ch);
-		else
-			printf(".");
-		ch++;
-	}
-
-	printf("\n");
-
-return;
-}
-
-/*
- * print packet payload data (avoid printing binary data)
- */
-void
-SnifferPlatform::print_payload(const u_char *payload, int len)
-{
-
-	int len_rem = len;
-	int line_width = 16;			/* number of bytes per line */
-	int line_len;
-	int offset = 0;					/* zero-based offset counter */
-	const u_char *ch = payload;
-
-	if (len <= 0)
-		return;
-
-	/* data fits on one line */
-	if (len <= line_width) {
-		print_hex_ascii_line(ch, len, offset);
-		return;
-	}
-
-	/* data spans multiple lines */
-	for ( ;; ) {
-		/* compute current line length */
-		line_len = line_width % len_rem;
-		/* print line */
-		print_hex_ascii_line(ch, line_len, offset);
-		/* compute total remaining */
-		len_rem = len_rem - line_len;
-		/* shift pointer to remaining bytes to print */
-		ch = ch + line_len;
-		/* add offset */
-		offset = offset + line_width;
-		/* check if we have line width chars or less */
-		if (len_rem <= line_width) {
-			/* print last line and get out */
-			print_hex_ascii_line(ch, len_rem, offset);
-			break;
-		}
-	}
-
-return;
-}
-
-
-
 std::stringstream printMacToStream(const u_char MACData[])
 {
 	std::stringstream os;
@@ -285,53 +183,46 @@ SnifferPlatform::UpdateConnectionTable(u_char *args, const struct pcap_pkthdr *h
 	pActiveEp->hasFlas = true;
 
 
+	PipeState pipeState = pPipe->state;
+
+
 
 	if ( pActiveEp->lastFlags.flagBits.syn && !pActiveEp->lastFlags.flagBits.ack){
 		if (smallerSentTheFlags){
-			pPipe->state = PipeState::SYN_SENT;
+			pipeState = PipeState::SYN_SENT;
 		}else{
-			pPipe->state = PipeState::SYN_RCVD;
+			pipeState = PipeState::SYN_RCVD;
 		}
 
 	}else 	if ( pActiveEp->lastFlags.flagBits.syn && pActiveEp->lastFlags.flagBits.ack){
-			pPipe->state = PipeState::ESTABLISHED;
+		pipeState = PipeState::ESTABLISHED;
 	}
 	else  	if ( pActiveEp->lastFlags.flagBits.fin && !pActiveEp->lastFlags.flagBits.ack){
 		if (smallerSentTheFlags){
-			if (PipeState::FIN_WAIT_1 == pPipe->state){
-				pPipe->state = PipeState::CLOSE_WAIT;
+			if (PipeState::FIN_WAIT_1 == pipeState){
+				pipeState = PipeState::CLOSE_WAIT;
 			}else{
-				pPipe->state = PipeState::FIN_WAIT_2;
+				pipeState = PipeState::FIN_WAIT_2;
 			}
 		}else{
-			if (PipeState::FIN_WAIT_2 == pPipe->state){
-				pPipe->state = PipeState::CLOSE_WAIT;
+			if (PipeState::FIN_WAIT_2 == pipeState){
+				pipeState = PipeState::CLOSE_WAIT;
 			}else{
-				pPipe->state = PipeState::FIN_WAIT_1;
+				pipeState = PipeState::FIN_WAIT_1;
 			}
 		}
 
 	}else  	if ( pActiveEp->lastFlags.flagBits.fin && pActiveEp->lastFlags.flagBits.ack){
-		pPipe->state =PipeState::CLOSED;
+		pipeState =PipeState::CLOSED;
 	}
 	else
 	{
-		pPipe->state = PipeState::ESTABLISHED;
+		pipeState = PipeState::ESTABLISHED;
 	}
 
+	pPipe->state = pipeState;
 
 
-if (configs.PrintTcpPayload){
-	/*
-	 * Print payload data; it might be binary, so don't just
-	 * treat it as a string.
-	 */
-	if (size_payload > 0) {
-		printf("   Payload (%d bytes):\n", size_payload);
-		print_payload(payload, size_payload);
-	}
-
-}
 
 //PrintPipes(); //update connection state after each reception
 
